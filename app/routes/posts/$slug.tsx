a5fe19c7f8c151afe76d83c6a6a96c94adbe6b30
Lucas Minter <lsminteris@gmail.com>
2022-06-08 15:18:55 -0700

    removed unused packages, added some styling

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -1,12 +1,8 @@
 import {marked} from 'marked'
-import TurndownService from 'turndown'
 import { LoaderFunction, json } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
 import client from '../../lib/sanity'
 import groq from 'groq'
-
-const turndownService = new TurndownService()
-
 
 export const loader: LoaderFunction = async ({params}) => {
   const {slug} = params
@@ -28,8 +24,8 @@ export default function PostRoute() {
   return (
     <main className="mx-auto max-w-4x1">
       <h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>
-      <div className="prose m-3">
-        <h4>{post.publishedAt} by {post.author._ref}</h4>
+      <div className="prose m-5 max-w-none">
+        <h4 className='text-center'>{post.publishedAt} by {post.author._ref}</h4>
         <div dangerouslySetInnerHTML={{__html: html}}/>
       </div>
     </main>
