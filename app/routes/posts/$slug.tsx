bbea1b14cf0bc05ce1e779ea01a7fb7662d45af9
Lucas Minter <lsminteris@gmail.com>
2022-06-10 14:48:01 -0700

    moved marked usage to the loader

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -14,17 +14,17 @@ export const loader: LoaderFunction = async ({params}) => {
     slug
   }`
   const post = await client.fetch(postData)
-  return json({post})
+  const html = marked(post.body)
+  return json({title: post.title, publishedDate: post.publishedAt, authorName: post.author.name, html})
 }
 
 export default function PostRoute() {
-  const {post} = useLoaderData()
-  const html = marked(post.body)
+  const {title, publishedDate, authorName, html} = useLoaderData()
   return (
     <main className="mx-auto max-w-4x1">
-      <h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>
+      <h1 className="my-6 border-b-2 text-center text-3xl">{title}</h1>
       <div className="prose m-5 max-w-none">
-        <h4 className='text-center'>Published on {post.publishedAt} by {post.author.name}</h4>
+        <h4 className='text-center'>Published on {publishedDate} by {authorName}</h4>
         <div dangerouslySetInnerHTML={{__html: html}}/>
       </div>
     </main>
